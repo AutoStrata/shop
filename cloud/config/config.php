3978a66d591e832c3f984d4c59a1a3eb1a1c586e
root <root@server.autostrata.ro>
2015-09-06 10:40:39 -0400

    AutoStrata begins

---
+++ cloud/config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc0rmry0a4eb',
+  'passwordsalt' => 'rhDaeBWa7bAXDl+Qp+GkSypT/6LVJa',
+  'secret' => '3o4R+VWWp6qGnN96ruJ6gXHFy5jthLaD45dRN0r9vfgTUZT1',
+  'trusted_domains' => 
+  array (
+    0 => 'cloud.autostrata.ro',
+  ),
+  'datadirectory' => '/home/autostrata/public_html/cloud/data',
+  'overwrite.cli.url' => 'http://cloud.autostrata.ro',
+  'dbtype' => 'mysql',
+  'version' => '8.1.1.3',
+  'dbname' => 'autostra_cloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'autostra_cloud',
+  'dbpassword' => 'OcIccOibEMUfLO02Ke',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
